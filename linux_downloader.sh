359f53688861ab17ecc7490c75e6b5d78f10ffd0
Luis Garcia <luigi311.lg@gmail.com>
2017-09-21 16:17:03 -0600

    Add centos

--- linux_downloader.sh
+++ linux_downloader.sh
@@ -7,3 +7,7 @@ wget $arch/$release/*iso.torrent -P .
 ubuntu=ftp://releases.ubuntu.com/releases/
 release=`curl -l $ubuntu | sort -n -r | awk NR==1`
 wget $ubuntu/$release/*iso.torrent -P .
+
+centos=ftp://mirror.rackspace.com/CentOS/
+release=`curl -l $centos | sort -n -r | awk NR==1`
+wget -r -nH --cut-dirs=5 --no-parent -A "*Everything*.torrent" $centos/$release/isos/x86_64/ -P $torrent_location/
