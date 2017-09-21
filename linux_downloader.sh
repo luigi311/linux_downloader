3c49a7ccdba12bd91dd2138e0ee2eef07e204c65
Luis Garcia <luigi311.lg@gmail.com>
2017-09-21 15:52:17 -0600

    Add ubuntu

--- linux_downloader.sh
+++ linux_downloader.sh
@@ -4,4 +4,6 @@ arch=ftp://mirror.rackspace.com/archlinux/iso/
 release=`curl -l $arch | sort -n -r | awk NR==1`
 wget $arch/$release/*iso.torrent -P .
 
-
+ubuntu=ftp://releases.ubuntu.com/releases/
+release=`curl -l $ubuntu | sort -n -r | awk NR==1`
+wget $ubuntu/$release/*iso.torrent -P .
